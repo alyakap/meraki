83bdbe2e69422d5e43993002a85ece5c43c61ff3
ZHanim <zilhan.be@gmail.com>
2019-12-18 16:24:11 +0100

    saving last  changes

--- Dec17-Js-Day2/require-issue/logger.js
+++ Dec17-Js-Day2/require-issue/logger.js
@@ -1,2 +1,9 @@
 var name = "David"
 console.log(name)
+
+function log(message){
+    console.log(message)
+}
+
+// module.export.log=log; exporting whole file as object
+module.export=log; //exporting just one function
