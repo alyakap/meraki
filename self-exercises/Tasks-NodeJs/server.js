ba6dc0fd492f7a25456f1c4644e9433849c64c04
ZHanim <zilhan.be@gmail.com>
2019-12-23 10:11:10 +0100

    checking req on console

--- self-exercises/Tasks-NodeJs/server.js
+++ self-exercises/Tasks-NodeJs/server.js
@@ -8,6 +8,7 @@ app.listen(port);
 //testing on browser... got "cannot get" message
 
 app.get('/',function(req,res){
+    console.log(req)
     console.log("homepage loaded")
     res.send('<h1>Tasks App</h1>')
 })
