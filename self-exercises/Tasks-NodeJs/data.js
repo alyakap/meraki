160cd1ac50409c38bd94dbe9fd22dfd4441c3bda
ZHanim <zilhan.be@gmail.com>
2019-12-23 10:33:29 +0100

    creating data.js with tasks array in it

---
+++ self-exercises/Tasks-NodeJs/data.js
@@ -0,0 +1,3 @@
+const tasks=["study OOP", "study js objects", "move on Kata 7 slowly", "more practice on array methods", "rest once in while", "study more", "be careful not to burn out"]
+
+module.exports(tasks)=tasks;
