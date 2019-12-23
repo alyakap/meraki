5dd3930a7078df1359862f7e9b00e44db7081059
ZHanim <zilhan.be@gmail.com>
2019-12-23 10:42:49 +0100

    getting all the tasks

--- self-exercises/Tasks-NodeJs/server.js
+++ self-exercises/Tasks-NodeJs/server.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const tasks=require('./data');
 const port=4567;
 
 const app=express(); //instance of express
@@ -10,4 +11,10 @@ app.listen(port);
 app.get('/',function(req,res){
     console.log("homepage loaded");
     res.send('<h1>Tasks App</h1>');
+})
+
+app.get('/tasks',function(req, res){
+    console.log('/tasks was requested'); 
+    const getTasks=tasks.map(task=>`<li>${task}</li>`)
+    res.send("<ul>Tasks</ul>"+getTasks)
 })
