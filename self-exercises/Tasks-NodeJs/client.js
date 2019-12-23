c13ddcde42e914d08ffaf7dfb3a1ae0c3c04331b
ZHanim <zilhan.be@gmail.com>
2019-12-23 15:42:31 +0100

    play around

--- self-exercises/Tasks-NodeJs/client.js
+++ self-exercises/Tasks-NodeJs/client.js
@@ -1,3 +1,17 @@
 console.log('message from client') 
 
-axios.get("http://localhost:4567/tasks").then(resp=>console.log(resp))
+axios.get("http://localhost:4567/tasks").then(handleData)
+
+  //HANDLEDATA
+  function handleData(axiosResponse) {
+    const listRef = document.querySelector("ul");
+    console.log(axiosResponse.data);
+    listRef.innerHTML = axiosResponse.data
+      .map(task => "<li>" + task.task + "</li>")
+      .join("");
+  }
+
+
+
+
+
