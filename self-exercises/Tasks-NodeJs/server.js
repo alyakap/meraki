494024ef48ce898bd0904f1b2dfa6bc785d74fea
ZHanim <zilhan.be@gmail.com>
2019-12-23 10:27:45 +0100

    remove on line

--- self-exercises/Tasks-NodeJs/server.js
+++ self-exercises/Tasks-NodeJs/server.js
@@ -8,7 +8,6 @@ app.listen(port);
 //testing on browser... got "cannot get" message
 
 app.get('/',function(req,res){
-    console.log(req)
-    console.log("homepage loaded")
-    res.send('<h1>Tasks App</h1>')
+    console.log("homepage loaded");
+    res.send('<h1>Tasks App</h1>');
 })
