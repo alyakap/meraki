76ba5ce7ca6aea313ddcbdaa3342f8a54586e4f1
ZHanim <zilhan.be@gmail.com>
2019-12-24 13:36:30 +0100

    data js created

---
+++ self-exercises/myfirstRestApi/app.js
@@ -0,0 +1,6 @@
+var express = require("express");
+var app = express();
+
+app.listen(3222, () => {
+    console.log("Server running on port 3000");
+   });
---
+++ self-exercises/myfirstRestApi/data.js
@@ -0,0 +1,8 @@
+const persons=[
+    {id=1, name="Hanim", lastname="Kapusuz"},
+    {id=2, name="David", lastname="Verhulst"},
+    {id=3, name="Askim", lastname="Kintziger"},
+    {id=4, name="Samira", lastname="Hamdaoui"},
+    {id=5, name="Murat", lastname="Kapusuz"},
+    {id=6, name="Zeynep", lastname="Ilhan"}
+]
