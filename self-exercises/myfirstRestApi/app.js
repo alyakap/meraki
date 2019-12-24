eab677e06584856256091ff41cacf9864e4ab690
ZHanim <zilhan.be@gmail.com>
2019-12-24 15:00:42 +0100

    commented out pbodyparser

--- self-exercises/myfirstRestApi/app.js
+++ self-exercises/myfirstRestApi/app.js
@@ -1,9 +1,9 @@
 const people=require('./data');
 const express = require("express");
 const app = express();
-const bodyParser=require('body-parser')
+// const bodyParser=require('body-parser')
 
-app.use(bodyParser.urlencoded({'extended':'true'}));          
+// app.use(bodyParser.urlencoded({'extended':'true'}));          
 //app.use(bodyParser.json());  
 
 
