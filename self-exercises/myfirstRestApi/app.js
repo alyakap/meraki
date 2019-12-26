77dec98590e2f3b958242d33af5db9f9088f043a
ZHanim <zilhan.be@gmail.com>
2019-12-26 13:13:48 +0100

    small bug

--- self-exercises/myfirstRestApi/app.js
+++ self-exercises/myfirstRestApi/app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true })) //??
 
 
 app.listen(port, () => {
-    console.log("Server running on port 3000");
+    console.log("Server running on port 3222");
     console.log(people); //checking my data 
    });
 
