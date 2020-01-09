72e94e32055870105bf1566d8d99885430e92a5f
HanimK <zilhan.be@gmail.com>
2020-01-09 20:28:10 +0100

    multible box issue

--- self-exercises/box-app/index.js
+++ self-exercises/box-app/index.js
@@ -4,6 +4,6 @@ import box from './components/Box';
 
 const app = document.getElementById('app');
 
-box(app, 300, 60, 59);
+const box1=box(app, 300, 60, 59);
 
-box(app, 40, 400, 300);
+const box2=box(app, 40, 400, 300);
