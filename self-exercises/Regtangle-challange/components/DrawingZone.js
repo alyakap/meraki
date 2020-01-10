403b021e50ed20ef7b462329a42aa95fc032829d
HanimK <zilhan.be@gmail.com>
2020-01-10 12:20:41 +0100

    fisrt two ins done

--- self-exercises/Regtangle-challange/components/DrawingZone.js
+++ self-exercises/Regtangle-challange/components/DrawingZone.js
@@ -1,10 +1,36 @@
+import randomcolor from 'randomcolor';
 class DrawingZone {
-    constructor(holder, width='300px', height='200px'){
+    constructor(holder, width="300", height="200", top="100", left="100"){
         this.holder=holder;
-        this.width=width;
+        this.width=width ;
         this.height=height;
+        this.top=top;
+        this.left=left;
+        this.zoneRef = this.generateHtml();
+        this.setupStyling();
+        this.setPosition();
+    }
+
+    generateHtml(){
+        this.holder.insertAdjacentHTML('beforeEnd', `<div class="zone" style="position:relative"> Drawing zone </div>`);
+        console.log("generate")
+        return this.holder.lastChild;
+    }
+    setupStyling(){
+        console.log(this.width)
+        this.zoneRef.style.width = this.width+ 'px';
+        this.zoneRef.style.height = this.height+ 'px';
+        this.zoneRef.style.backgroundColor=randomcolor();
+    }
+    setPosition(){
+        this.zoneRef.style.left= this.left+'px';
+        this.zoneRef.style.top = this.top+'px';
+    }
+    changeSize(givenWidth, givenHeight){
+        this.zoneRef.style.width = givenWidth+ 'px';
+        this.zoneRef.style.height = givenHeight+ 'px';
     }
 
 }
 
-export default (holder, width, height)= new DrawingZone(holder, width, height)
+export default (holder, width, height, top, left)=> new DrawingZone(holder, width, height, top, left)
--- self-exercises/Regtangle-challange/index.js
+++ self-exercises/Regtangle-challange/index.js
@@ -1,4 +1,10 @@
 import drawingZone from './components/DrawingZone';
-import regtangle from './components/Regtangle';
+//import regtangle from './components/Regtangle';
 
 const app = document.getElementById('app');
+
+const zone1=drawingZone(app);
+
+console.log(zone1.zoneRef)
+
+zone1.changeSize(500, 100);
