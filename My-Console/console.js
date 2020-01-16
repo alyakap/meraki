5da97f5ee92629bd1af45e33a78bf36fed22776b
HanimK <zilhan.be@gmail.com>
2020-01-16 09:03:59 +0100

    codewars challenges

--- My-Console/console.js
+++ My-Console/console.js
@@ -7,14 +7,17 @@
 
 // console.log(result)
 
-function nextBigger(n){
-  let w=n.toString().split("").map(Number)
-  let v1=w.reduce(function permute(res, item, key, arr) {
-    return res.concat(arr.length > 1 && arr.slice(0, key).concat(arr.slice(key + 1)).reduce(permute, []).map(function(perm) { return [item].concat(perm); }) || item);}, []);
-  let v2=v1.map(x=>parseInt(x.join(""))).sort((a,b)=>b-a)
-    let ind=v2.indexOf(n)-1
-    
-    return v2[ind];
- 
+
+
+// function isValidWalk(walk) {
+//   console.log(walk.join("").replace(/ns/g,'').replace(/we/g,''));
+// }
+// isValidWalk(['n','s','n','s','n','s','n','s','n','s'])
+// isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e'])
+// isValidWalk(['n','n','n','s','n','s','n','s','n','s']);
+
+function highAndLow(numbers){
+  return `${Math.max(numbers.split(' ').map(Number))} ${Math.min(numbers.split(' ').map(Number))}`
 }
-console.log(nextBigger(531))
+console.log(highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"))
+console.log(Mat("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6".split(' ').map(Number)))
