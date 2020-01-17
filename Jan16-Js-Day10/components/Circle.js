9ded14bcadac6e38c378ae0ff64727f694ad1a3d
David Verhulst <david.verhulst@cronos.be>
2020-01-17 14:30:04 +0100

    updated review

--- Jan16-Js-Day10/components/Circle.js
+++ Jan16-Js-Day10/components/Circle.js
@@ -1,5 +1,6 @@
 import randomcolor from 'randomcolor';
-import Dialog from './Dialog';
+//import Dialog from './Dialog';
+import { confirm } from 'vanilla-dialogs'
 
 export default class Circle {
     constructor(holder, width = 100, height = 100) {
@@ -17,6 +18,22 @@ export default class Circle {
         return this.holder.lastChild;
     }
     setupStyling() {
+
+        // const styleObject = {
+        //     position: `absolute`,
+        //     top: `${this.y}px`,
+        //     left: `${this.x}px`,
+        //     width: `${this.size}px`,
+        //     height: `${this.size}px`,
+        //     marginLeft: `-${this.size / 2}px`,
+        //     marginTop: `-${this.size / 2}px`,
+        //     borderRadius: `50%`,
+        //     backgroundColor: `${this.bgc}`,
+        //     cursor: `pointer`
+        // }
+
+        // Object.assign(this.htmlRef.style, styleObject);
+
         this.cirRef.style.width = this.width + 'px';
         this.cirRef.style.height = this.height + 'px';
         this.cirRef.style.left = '50%';
@@ -30,18 +47,28 @@ export default class Circle {
     onClickHandler(e) {
         e.stopPropagation(); //stop parent's(body) element click event
         let size = this.getRandomRange(50, 200)
-        this.cirRef.style.left = `${this.getRandomRange(0, this.holder.offsetWidth)}px`;
-        this.cirRef.style.top = `${this.getRandomRange(0, this.holder.offsetHeight)}px`;
-        this.cirRef.style.width = size + 'px';
-        this.cirRef.style.height = size + 'px';
+        this.width = size;
+        this.height = size;
+        // this.cirRef.style.margin = `${-(this.height / 2)}px 0px 0px ${-(this.width / 2)}px`
+
+        // this.cirRef.style.width = size + 'px';
+        // this.cirRef.style.height = size + 'px';
+        this.setupStyling()
+        this.cirRef.style.left = `${this.getRandomRange(this.width / 2, window.innerWidth - this.width / 2)}px`;
+        this.cirRef.style.top = `${this.getRandomRange(this.height / 2, window.innerHeight - this.height / 2)}px`;
         this.cirRef.style.backgroundColor = randomcolor();
         if (e.altKey) {
             console.log("alt click")
-            new Dialog(this.holder, this.onConfirm.bind(this))
+            // new Dialog(this.holder, this.onConfirm.bind(this))
+            confirm('You want to erase the circle?').then(result => {
+                if (result) {
+                    this.onConfirm()
+                }
+            })
         }
     }
     getRandomRange(min, max) {
-        return Math.floor((Math.random() * (max + 1 - min)) + min);
+        return Math.floor((Math.random() * (max - min)) + min);
     }
 
     onConfirm() {
