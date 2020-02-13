9ac8cc357d644123c929c5c018a68aab536d420d
HanimK <zilhan.be@gmail.com>
2020-02-13 14:13:18 +0100

    adding html elements

--- 2-React-Jan/5-gif-app-new/src/App.js
+++ 2-React-Jan/5-gif-app-new/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 function App() {
-  return <></>;
+  return (
+    <div class="input-group md-form form-sm form-2 pl-0">
+      <input
+        class="form-control my-0 py-1 pink-border"
+        type="text"
+        placeholder="Search"
+        aria-label="Search"
+      />
+      <div class="input-group-append">
+        <span class="input-group-text pink lighten-3" id="basic-text1">
+          <i class="fas fa-search text-grey" aria-hidden="true"></i>
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default App;
