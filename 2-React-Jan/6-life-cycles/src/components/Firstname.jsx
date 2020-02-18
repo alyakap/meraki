ccfa3872ec2a014524e193b8c10d2a224445ad1d
HanimK <zilhan.be@gmail.com>
2020-02-18 12:07:10 +0100

    aading console.logs

--- 2-React-Jan/6-life-cycles/src/components/Firstname.jsx
+++ 2-React-Jan/6-life-cycles/src/components/Firstname.jsx
@@ -4,8 +4,13 @@ export default class Firstname extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+    console.log("Firstname Constructor");
+  }
+  componentDidMount() {
+    console.log("Firstname componentDidMount");
   }
   render() {
+    console.log("Firstname Render");
     return <h1>{this.props.person.name}</h1>;
   }
 }
