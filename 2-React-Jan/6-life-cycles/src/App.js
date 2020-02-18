3a3b4b4f79f531b2b688a0158c1aac65835f63f9
HanimK <zilhan.be@gmail.com>
2020-02-18 14:41:31 +0100

    pasted Davids' change

--- 2-React-Jan/6-life-cycles/src/App.js
+++ 2-React-Jan/6-life-cycles/src/App.js
@@ -1,19 +1,47 @@
-import React from "react";
 import Firstname from "./components/Firstname";
-function App() {
-  const person = {
-    name: "Hanim",
-    lastname: "Kapusuz",
-    age: 34
-  };
-  return (
-    <>
-      <Firstname person={person} />
-      <button>David</button>
-      <button>38</button>
-      <button>Toggle</button>
-    </>
-  );
-}
+import React, { Component } from "react";
 
-export default App;
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.firstNameHandler = this.firstNameHandler.bind(this);
+    this.ageHandler = this.ageHandler.bind(this);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      person: {
+        firstName: "Hanim",
+        lastName: "Kapusuz",
+        age: 25
+      },
+      visible: true
+    };
+  }
+  firstNameHandler() {
+    this.setState({
+      ...this.state,
+      person: { ...this.state.person, firstName: "David" }
+    });
+  }
+  ageHandler() {
+    this.setState({
+      ...this.state,
+      person: { ...this.state.person, age: 38 }
+    });
+  }
+  toggle() {
+    this.setState({
+      ...this.state,
+      visible: !this.state.visible
+    });
+  }
+  render() {
+    return (
+      <div>
+        {this.state.visible && <Firstname person={this.state.person} />}
+        <button onClick={this.firstNameHandler}>set firstName to David</button>
+        <button onClick={this.ageHandler}>set age to 38</button>
+        <button onClick={this.toggle}>toggle component visibility</button>
+      </div>
+    );
+  }
+}
--- 2-React-Jan/6-life-cycles/src/components/Firstname.jsx
+++ 2-React-Jan/6-life-cycles/src/components/Firstname.jsx
@@ -4,13 +4,54 @@ export default class Firstname extends Component {
   constructor(props) {
     super(props);
     this.state = {};
-    console.log("Firstname Constructor");
+    console.log("1) constructor => COMPONENT IS INSTANTIATED");
+  }
+  static getDerivedStateFromProps(props, state) {
+    console.log(
+      "2) getDerivedStateFromProps => WHERE WE UPDATE OUR LOCAL STATE BASED ON CHANGED PROPS"
+    );
+    return state;
   }
   componentDidMount() {
-    console.log("Firstname componentDidMount");
+    console.log(
+      "4) componentDidMount => WHERE WE CAN ACCESS ELEMENTS ADDED TO DOM, WHERE WE CAN START API-CALLS"
+    );
+  }
+  componentWillUnmount() {
+    console.log(
+      "componentWillUnmount => JUST BEFORE REMOVING FROM DOM, TO CLEANUP LISTENERS, TIMERS"
+    );
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log(
+      "shouldComponentUpdate => WHERE WE CAN DECIDE TO RE-RENDER OR NOT BASED ON NEXTPROPS/NEXTSTATE"
+    );
+    const shouldRerender =
+      nextProps.person.firstName !== this.props.person.firstName;
+    if (shouldRerender) {
+      console.log(
+        "   ---> component has to rerender because firstName changed"
+      );
+    } else {
+      console.log(
+        "   ---> component will NOT rerender because firstName still equal"
+      );
+    }
+    return shouldRerender;
+  }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log(
+      "getSnapshotBeforeUpdate => CALLED AFTER RENDER WITH ABILITY TO DO CALCULATION BASED ON PREVIOUS PROPS/STATE, WHAT IS RETURNED CAN BE CALLED IN COMPONENTDIDUPDATE"
+    );
+    return null;
+  }
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log(
+      "componentDidUpdate => CALLED AFTER GETSNAPSHOTBEFOREUPDATE WITH ACCESS TO RETURNED VALUE FROM GETSNAPSHOTBEFOREUPDATE"
+    );
   }
   render() {
-    console.log("Firstname Render");
-    return <h1>{this.props.person.name}</h1>;
+    console.log("3) render => WHERE ACTUAL ADDING ELEMENTS TO DOM HAPPENS");
+    return <h1>{this.props.person.firstName}</h1>;
   }
 }
